d55e756a97c1f2c7f41f1b4dd0bc775de960a371
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2020-04-22 17:11:13 -0300

    feat(module7): add dice random application

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,51 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  void getDiceFace() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image(
+                image: AssetImage('images/dice$leftDiceNumber.png'),
+              ),
+              onPressed: () {
+                setState(() {
+                  getDiceFace();
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image(
+                image: AssetImage('images/dice$rightDiceNumber.png'),
+              ),
+              onPressed: () {
+                setState(() {
+                  getDiceFace();
+                });
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
